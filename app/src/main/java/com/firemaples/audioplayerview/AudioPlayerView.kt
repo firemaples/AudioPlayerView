8290cc5d30bd22a43c0646eeca7b62537bd8612c
firemaples <firemaples@gmail.com>
2020-07-02 18:45:16 +0800

    Implement the display

---
+++ app/src/main/java/com/firemaples/audioplayerview/AudioPlayerView.kt
@@ -0,0 +1,306 @@
+package com.firemaples.audioplayerview
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+
+class AudioPlayerView : View {
+    private var progressColor: String = "#4c5acff2"
+    private var buttonBgColor: String = "#5acff2"
+    private var buttonColor: String = "#ffffff"
+    private var textColor: String = "#000000"
+
+    private var buttonBgSize: Float = 22.toPx()
+    private var buttonSize: Float = 10.toPx()
+    private var buttonPaddingLeft: Float = 15.toPx()
+    private var textPaddingLeft: Float = 9.toPx()
+    private var textSize: Float = 16.toPx()
+
+    private var progress: Float = 0.5f
+    private var buttonState: ButtonState = ButtonState.Play
+
+    //    private var drawingTopLeftRadius: Float = 18.toDp()
+//    private var drawingTopRightRadius: Float = 18.toDp()
+    private var drawingTopLeftRadius: Float = 0f
+    private var drawingTopRightRadius: Float = 0f
+    private var drawingBottomRightRadius: Float = 18.toPx()
+    private var drawingBottomLeftRadius: Float = 18.toPx()
+
+    private val progressPaint: Paint = Paint().apply {
+        color = Color.parseColor(progressColor)
+    }
+    private val buttonBgPaint: Paint = Paint().apply {
+        color = Color.parseColor(buttonBgColor)
+    }
+    private val buttonPaint: Paint = Paint().apply {
+        color = Color.parseColor(buttonColor)
+    }
+    private val textPaint: Paint = Paint().apply {
+        color = Color.parseColor(textColor)
+        textSize = this@AudioPlayerView.textSize
+        isAntiAlias = true
+    }
+
+    private val progressDrawable: Drawable? =
+        ContextCompat.getDrawable(context, R.drawable.progress)?.apply {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                colorFilter = PorterDuffColorFilter(
+                    Color.WHITE,
+                    PorterDuff.Mode.SRC_ATOP
+                )
+            } else {
+                setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+            }
+        }
+    private var progressButtonDegree: Float = 0f
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    fun updateProgress(progress: Float) {
+        this.progress = progress.coerceAtMost(1f).coerceAtLeast(0f)
+        postInvalidate()
+    }
+
+    fun getCurrentButtonState(): ButtonState = buttonState
+
+    fun updateButtonState(buttonState: ButtonState) {
+        this.buttonState = buttonState
+        postInvalidate()
+    }
+
+    fun updateProgressButtonDegree(degree: Float) {
+        this.progressButtonDegree = degree.coerceAtLeast(0f).coerceAtMost(360f)
+        postInvalidate()
+    }
+
+    fun updateRadius(
+        topLeftRadius: Int,
+        topRightRadius: Int,
+        bottomRightRadius: Int,
+        bottomLeftRadius: Int
+    ) {
+        this.drawingTopLeftRadius = topLeftRadius.toPx()
+        this.drawingTopRightRadius = topRightRadius.toPx()
+        this.drawingBottomRightRadius = bottomRightRadius.toPx()
+        this.drawingBottomLeftRadius = bottomLeftRadius.toPx()
+        postInvalidate()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        clipRegion(canvas)
+
+        drawProgressBar(canvas, progress)
+
+        drawButton(canvas)
+
+        drawText(canvas, "00:06")
+
+//        drawTest(canvas)
+
+    }
+
+    private fun drawTest(canvas: Canvas) {
+
+        canvas.drawRect(RectF(0f, 0f, width.toFloat(), height.toFloat()), progressPaint)
+    }
+
+    private fun clipRegion(canvas: Canvas) {
+        val path = getPathOfRoundedRectF(
+            rect = RectF(0f, 0f, width.toFloat(), height.toFloat()),
+            topLeftRadius = drawingTopLeftRadius,
+            topRightRadius = drawingTopRightRadius,
+            bottomLeftRadius = drawingBottomLeftRadius,
+            bottomRightRadius = drawingBottomRightRadius
+        )
+        canvas.clipPath(path)
+    }
+
+    private fun drawProgressBar(canvas: Canvas, progress: Float) {
+        if (progress <= 0) return
+
+        val rect = RectF(0f, 0f, width * progress, height.toFloat())
+
+        val path = when {
+            progress >= 1 -> {
+                getPathOfRoundedRectF(
+                    rect = rect
+                )
+            }
+            else -> {
+                getPathOfRoundedRectF(
+                    rect = rect,
+                    topRightRadius = height / 2f,
+                    bottomRightRadius = height / 2f
+                )
+            }
+        }
+
+        canvas.drawPath(path, progressPaint)
+    }
+
+    private fun drawButton(canvas: Canvas) {
+        //draw button background
+        val bgPadding = (height - buttonBgSize) / 2
+        val bgRect = RectF(
+            buttonPaddingLeft,
+            bgPadding,
+            buttonPaddingLeft + buttonBgSize,
+            height - bgPadding
+        )
+        canvas.drawOval(bgRect, buttonBgPaint)
+
+        //draw button
+        val btPadding = (bgRect.width() - buttonSize) / 2
+        val btRect = RectF(
+            bgRect.left + btPadding,
+            bgRect.top + btPadding,
+            bgRect.right - btPadding,
+            bgRect.bottom - btPadding
+        )
+
+
+        when (buttonState) {
+            ButtonState.Play -> {
+                val delta = btRect.width() / 6f
+                with(Path()) {
+                    moveTo(btRect.left + delta, btRect.top)
+                    lineTo(btRect.right, btRect.centerY())
+                    lineTo(btRect.left + delta, btRect.bottom)
+                    close()
+
+                    canvas.drawPath(this, buttonPaint)
+                }
+            }
+            ButtonState.Stop -> {
+                val path = getPathOfRoundedRectF(
+                    btRect,
+                    topLeftRadius = 2.toPx(),
+                    topRightRadius = 2.toPx(),
+                    bottomRightRadius = 2.toPx(),
+                    bottomLeftRadius = 2.toPx()
+                )
+
+                canvas.drawPath(path, buttonPaint)
+            }
+            ButtonState.Progress -> {
+                if (progressDrawable != null) {
+                    progressDrawable.setBounds(
+                        btRect.left.toInt(),
+                        btRect.top.toInt(),
+                        btRect.right.toInt(),
+                        btRect.bottom.toInt()
+                    )
+                    canvas.save()
+                    canvas.rotate(progressButtonDegree, btRect.centerX(), btRect.centerY())
+                    progressDrawable.draw(canvas)
+                    canvas.restore()
+                }
+            }
+        }
+    }
+
+    private fun drawText(canvas: Canvas, text: String) {
+        val bound = Rect()
+        textPaint.getTextBounds(text, 0, text.length, bound)
+
+        canvas.drawText(
+            text,
+            buttonPaddingLeft + buttonBgSize + textPaddingLeft,
+            (height - bound.height()) / 2f + bound.height(),
+            textPaint
+        )
+    }
+
+    private fun getPathOfRoundedRectF(
+        rect: RectF,
+        topLeftRadius: Float = 0f,
+        topRightRadius: Float = 0f,
+        bottomRightRadius: Float = 0f,
+        bottomLeftRadius: Float = 0f
+    ): Path {
+        val tlRadius = topLeftRadius.coerceAtLeast(0f)
+        val trRadius = topRightRadius.coerceAtLeast(0f)
+        val brRadius = bottomRightRadius.coerceAtLeast(0f)
+        val blRadius = bottomLeftRadius.coerceAtLeast(0f)
+
+        with(Path()) {
+            moveTo(rect.left + tlRadius, rect.top)
+
+            //setup top border
+            lineTo(rect.right - trRadius, rect.top)
+
+            //setup top-right corner
+            arcTo(
+                RectF(
+                    rect.right - trRadius * 2f,
+                    rect.top,
+                    rect.right,
+                    rect.top + trRadius * 2f
+                ), -90f, 90f
+            )
+
+            //setup right border
+            lineTo(rect.right, rect.bottom - trRadius)
+
+            //setup bottom-right corner
+            arcTo(
+                RectF(
+                    rect.right - brRadius * 2f,
+                    rect.bottom - brRadius * 2f,
+                    rect.right,
+                    rect.bottom
+                ), 0f, 90f
+            )
+
+            //setup bottom border
+            lineTo(rect.left + blRadius, rect.bottom)
+
+            //setup bottom-left corner
+            arcTo(
+                RectF(
+                    rect.left,
+                    rect.bottom - blRadius * 2f,
+                    rect.left + blRadius * 2f,
+                    rect.bottom
+                ), 90f, 90f
+            )
+
+            //setup left border
+            lineTo(rect.left, rect.top + tlRadius)
+
+            //setup top-left corner
+            arcTo(
+                RectF(
+                    rect.left,
+                    rect.top,
+                    rect.left + tlRadius * 2f,
+                    rect.top + tlRadius * 2f
+                ),
+                180f,
+                90f
+            )
+
+            close()
+
+            return this
+        }
+    }
+
+    private fun Int.toPx(): Float = this * Resources.getSystem().displayMetrics.density
+
+    enum class ButtonState {
+        Play, Stop, Progress
+    }
+}
--- app/src/main/java/com/firemaples/audioplayerview/MainActivity.kt
+++ app/src/main/java/com/firemaples/audioplayerview/MainActivity.kt
@@ -2,11 +2,56 @@ package com.firemaples.audioplayerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        audioPlayerView.setOnClickListener {
+            val state = audioPlayerView.getCurrentButtonState()
+
+            var next = AudioPlayerView.ButtonState.values().indexOf(state) + 1
+
+            if (next >= AudioPlayerView.ButtonState.values().size) {
+                next = 0
+            }
+
+            audioPlayerView.updateButtonState(buttonState = AudioPlayerView.ButtonState.values()[next])
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                audioPlayerView.updateProgress(progress / 100f)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                audioPlayerView.updateProgressButtonDegree(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        sw_replyMode.setOnCheckedChangeListener { _, checked ->
+            if (checked)
+                audioPlayerView.updateRadius(0, 0, 18, 18)
+            else
+                audioPlayerView.updateRadius(18, 18, 18, 18)
+        }
     }
 }
